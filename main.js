7b1ceaa875abb2d7a15d2dbde5072934e29f14fa
marwan <marwanbouhamid20@gmail.com>
2023-08-11 18:45:59 +0100

    first commit

--- main.js
+++ main.js
@@ -1,382 +1,115 @@
- 
- 
- 
- 
- 
- const words=[
-    "Hello",
-    "Code",
-    "JvaScript",
-    "Town",
-    "Country",
-    "Testing",
-    "Youtube",
-    "Linkedin",
-    "Twitter",
-    "Github",
-    "Html",
-    "Internet",
-    "Python",
-    "Scale",
-    "Destructuring",
-    "Paradigm",
-    "Styling",
-    "Cascade",
-    "Documentation",
-    "Coding",
-    "Funny",
-    "Working",
-    "Dependencies",
-    "Task",
-    "Runner",
-    "Roles",
-    "Test",
-    "Rust",
-    "Playing",
-    "Programming",
- ];
-let WordsHard = [
-    "Programming",
-    "Dependencies",
-    "Documentation",
-    "Destructuring",
-    "Linkedin"
-]
-let wordsEasy =[
-    "Task",
-    "Runner",
-    "Roles",
-    "Test",
-    "Rust",
-    "Playing",
-    "Coding",
-    "Funny",
-    "Working",
-    "Paradigm",
-    "Styling",
-    "Town",
-    "Country",
-    "Testing",
-    "Youtube",
-    "Linkedin",
-    "Twitter",
-    "Github",
-    "Html",
-    "Internet",
-    "Python",
-    "Scale",
-    "Cascade",
-    "Hello",
-    "Code",
-]
- let iconSetting = document.querySelector(".game .material-symbols-outlined");
- iconSetting.onclick=function(){
-     document.querySelector(".menu").classList.toggle("add-display-none")
- }
- let levelGame1 = document.querySelector(".levels-game1");
- let levelGame2 = document.querySelector(".levels-game2");
- let levelGame3= document.querySelector(".levels-game3");
-
- const lvls ={
-    "Easy":5 ,
-    "Normal":3 ,
-    "Hard":2
- };
-
-
- 
-//  let defaultNormal = "Easy";     
-let StartButton =document.querySelector(".start");
-let lvlNameSpan =document.querySelector(".message .lvl");
-  let secondsNameSpan =document.querySelector(".message .seconds");
-  let theWord =document.querySelector(".the-word");
-  let upcomingWords =document.querySelector(".upcoming-words");
-  let input =document.querySelector(".input");
-  let timeLeftSpan =document.querySelector(".time span");
-  let scoreGot =document.querySelector(".score .got");
-  let scoreTotal =document.querySelector(".score .total");
-  let finishMessage =document.querySelector(".finish");
-
-  let defaultNormal = "Normal";
-  let defaultNormalValue = lvls[defaultNormal];
-//   StartButton.onclick =function(){
-//     this.remove();
-//     input.focus();
-//     getAttribute()
-
-// }
-
-
-
-levelGame1.onclick=function(){
-       defaultNormal = "Easy";
-      defaultNormalValue = lvls[defaultNormal];
-      lvlNameSpan.innerHTML = defaultNormal;
-      secondsNameSpan.innerHTML = defaultNormalValue;
-      timeLeftSpan.innerHTML = defaultNormalValue;
-      scoreTotal.innerHTML= wordsEasy.length;
-      window.localStorage.setItem("defaultNormal",defaultNormal);
-      StartButton.onclick =function(){
-        this.remove();
-        input.focus();
-        getAttribute()
-    
+const questions = [
+    {
+        imageSrc: 'question1.jpg',
+        questionText: 'ما هو عاصمة فرنسا؟',
+        options: ['باريس', 'لندن', 'مدريد'],
+        correctOption: 'باريس'
+    },
+    {
+        imageSrc: 'question2.jpg',
+        questionText: 'ما هو أكبر كوكب في النظام الشمسي؟',
+        options: ['الزهرة', 'المشتري', 'الأرض'],
+        correctOption: 'المشتري'
+    },
+    {
+        imageSrc: 'question3.jpg',
+        questionText: 'ما هي أكبر دولة في العالم',
+        options: ['فرنسا', 'الصين', 'روسيا'],
+        correctOption: 'روسيا'
     }
-           function getAttribute(){
-            let random = wordsEasy[Math.floor(Math.random() * wordsEasy.length)];
-            let WordIndexOf = wordsEasy.indexOf(random);
-            words.splice(WordIndexOf,1);
-            theWord.innerHTML=random;
-            upcomingWords.innerHTML="";
-            for (let index = 0; index < wordsEasy.length; index++) {
-                let div = document.createElement("div");
-                let text = document.createTextNode(wordsEasy[index]);
-                div.appendChild(text);
-                upcomingWords.appendChild(div)
-                
-            }
-            startplay();
-        }
-        
-        function startplay(){
-            timeLeftSpan.innerHTML=defaultNormalValue;
-            let start = setInterval(()=>{
-                timeLeftSpan.innerHTML--;
-                if(timeLeftSpan.innerHTML === "0"){
-                    clearInterval(start)
-                    if (theWord.innerHTML.toLowerCase() === input.value.toLowerCase()) {
-                        input.value ='';
-                        scoreGot.innerHTML++;
-                        if(wordsEasy.length > 0){
-                            getAttribute()
-                        }else{
-                            let span= document.createElement("span");
-                            span.className="good";
-                            let textSpan = document.createTextNode("Congratz");
-                            span.appendChild(textSpan);
-                            finishMessage.appendChild(span);
-                            upcomingWords.remove()
-                        }
-                    }else{
-                        let res =document.createElement("div");
-                        res.className="reset";
-                        let textReset = document.createTextNode("Reset");
-                        res.appendChild(textReset);
-                        // resetGame.appendChild(reset);
-                        finishMessage.appendChild(res)
-                    }
-                }
-            },1000)
-        }
-        
-      }
-  levelGame2.onclick=function(){
-
-        defaultNormal = "Normal";
-       defaultNormalValue = lvls[defaultNormal];
-       lvlNameSpan.innerHTML = defaultNormal;
-       secondsNameSpan.innerHTML = defaultNormalValue;
-       timeLeftSpan.innerHTML = defaultNormalValue;
-       scoreTotal.innerHTML= words.length;
-       window.localStorage.setItem("defaultNormal",defaultNormal);
-         StartButton.onclick =function(){
-    this.remove();
-    input.focus();
-    getAttribute()
-
-}
-       function getAttribute(){
-        let random = words[Math.floor(Math.random() * words.length)];
-        let WordIndexOf = words.indexOf(random);
-        words.splice(WordIndexOf,1);
-        theWord.innerHTML=random;
-        upcomingWords.innerHTML="";
-        for (let index = 0; index < words.length; index++) {
-            let div = document.createElement("div");
-            let text = document.createTextNode(words[index]);
-            div.appendChild(text);
-            upcomingWords.appendChild(div)
-            
-        }
-        startplay();
+    ,{
+        imageSrc: 'question4.jpg',
+        questionText: 'ما هي أجمل جزر في العالم؟',
+        options: ['الكنز', 'المالديف', 'هواي'],
+        correctOption: 'المالديف'
     }
-    
-    function startplay(){
-        timeLeftSpan.innerHTML=defaultNormalValue;
-        let start = setInterval(()=>{
-            timeLeftSpan.innerHTML--;
-            if(timeLeftSpan.innerHTML === "0"){
-                clearInterval(start)
-                if (theWord.innerHTML.toLowerCase() === input.value.toLowerCase()) {
-                    input.value ='';
-                    scoreGot.innerHTML++;
-                    if(words.length > 0){
-                        getAttribute()
-                    }else{
-                        let span= document.createElement("span");
-                        span.className="good";
-                        let textSpan = document.createTextNode("Congratz");
-                        span.appendChild(textSpan);
-                        finishMessage.appendChild(span);
-                        upcomingWords.remove()
-                    }
-                }else{
-                    let span= document.createElement("span")
-                    span.className ="bad";
-                    let spanText = document.createTextNode("Game Over");
-                    span.appendChild(spanText);
-                    finishMessage.appendChild(span)
-                }
-            }
-        },1000)
+    ,{
+        imageSrc: 'question5.jpg',
+        questionText: 'ما هي أكبر جزيرة في إفريقيا',
+        options: ['الجزائر', 'موريتانيا', 'المغرب'],
+        correctOption: 'الجزائر'
     }
-    
-  }
-  levelGame3.onclick=function(){
-    
-
-        defaultNormal = "Hard";
-       defaultNormalValue = lvls[defaultNormal];
-       lvlNameSpan.innerHTML = defaultNormal;
-       secondsNameSpan.innerHTML = defaultNormalValue;
-       timeLeftSpan.innerHTML = defaultNormalValue;
-       scoreTotal.innerHTML= WordsHard.length;
-       window.localStorage.setItem("defaultNormal",defaultNormal);
-       StartButton.onclick =function(){
-        this.remove();
-        input.focus();
-        getAttribute()
-    
+    ,{
+        imageSrc: 'question6.jpg',
+        questionText: 'من  هو المنتخب الدي فاز في كأس العالم عام 2018',
+        options: ['الأرجنتين', 'ألمانيا', 'فرنسا'],
+        correctOption: 'فرنسا'
     }
-       function getAttribute(){
-        let random = WordsHard[Math.floor(Math.random() * WordsHard.length)];
-        let WordIndexOf = WordsHard.indexOf(random);
-        words.splice(WordIndexOf,1);
-        theWord.innerHTML=random;
-        upcomingWords.innerHTML="";
-        for (let index = 0; index < WordsHard.length; index++) {
-            let div = document.createElement("div");
-            let text = document.createTextNode(WordsHard[index]);
-            div.appendChild(text);
-            upcomingWords.appendChild(div)
-            
-        }
-        startplay();
+    ,{
+        imageSrc: 'question7.jpg',
+        questionText: 'كم عدد الأهداف التي سجلة في نهائي كأس العالم 2022',
+        options: ['2', '4', '6'],
+        correctOption: '6'
     }
-
-
-
-
-    function startplay(){
-        timeLeftSpan.innerHTML=defaultNormalValue;
-        let start = setInterval(()=>{
-            timeLeftSpan.innerHTML--;
-            if(timeLeftSpan.innerHTML === "0"){
-                clearInterval(start)
-                if (theWord.innerHTML.toLowerCase() === input.value.toLowerCase()) {
-                    input.value ='';
-                    scoreGot.innerHTML++;
-                    if(WordsHard.length > 0){
-                        getAttribute()
-                    }else{
-                        let span= document.createElement("span");
-                        span.className="good";
-                        let textSpan = document.createTextNode("Congratz");
-                        span.appendChild(textSpan);
-                        finishMessage.appendChild(span);
-                        upcomingWords.remove()
-                    }
-                }else{
-                    let res =document.createElement("div");
-                    res.className="reset";
-                    let textReset = document.createTextNode("Reset");
-                    res.appendChild(textReset);
-                    // resetGame.appendChild(reset);
-                    finishMessage.appendChild(res)
-
-
-                    // let span= document.createElement("span")
-                    // span.className ="bad";
-                    // let spanText = document.createTextNode("Game Over");
-                    // span.appendChild(spanText);
-                    // finishMessage.appendChild(span)
-                }
-            }
-        },1000)
+    ,{
+        imageSrc: 'question8.jpg',
+        questionText: '25 * 4 /  5',
+        options: ['20', '15', '40'],
+        correctOption: '20'
     }
-    
-    
+    ,{
+        imageSrc: 'question9.jpg',
+        questionText: '1000 - 500 -200 -300 + 1',
+        options: ['1', '200', '201'],
+        correctOption: '1'
     }
+    // يمكنك إضافة المزيد من الأسئلة هنا
+];
 
+let currentQuestionIndex = 0;
+let score = 0;
 
+const imageElement = document.getElementById('image');
+const questionTextElement = document.getElementById('questionText');
+const optionAElement = document.getElementById('optionA');
+const optionBElement = document.getElementById('optionB');
+const optionCElement = document.getElementById('optionC');
+const optionDElement = document.getElementById('optionD');
+const optionEElement = document.getElementById('optionE');
+const optionFElement = document.getElementById('optionF');
+const optionGElement = document.getElementById('optionG');
+const optionHElement = document.getElementById('optionH');
+const optionKElement = document.getElementById('optionK');
+const scoreElement = document.getElementById('score');
 
-
-
-
-    lvlNameSpan.innerHTML = defaultNormal;
-    secondsNameSpan.innerHTML = defaultNormalValue;
-    timeLeftSpan.innerHTML = defaultNormalValue;
-    scoreTotal.innerHTML= words.length;
-    window.localStorage.setItem("defaultNormal",defaultNormal);
-input.onpaste = function(){
-    return false;
+function displayQuestion(index) {
+    const question = questions[index];
+    imageElement.src = question.imageSrc;
+    questionTextElement.textContent = question.questionText;
+    optionAElement.textContent = question.options[0];
+    optionBElement.textContent = question.options[1];
+    optionCElement.textContent = question.options[2];
+    optionDElement.textContent = question.options[3];
+    optionEElement.textContent = question.options[4];
+    optionFElement.textContent = question.options[5];
+    optionGElement.textContent = question.options[6];
+    optionHElement.textContent = question.options[7];
+    optionKElement.textContent = question.options[8];
 }
 
-
-StartButton.onclick =function(){
-    this.remove();
-    input.focus();
-    getAttribute()
-
-}
-       function getAttribute(){
-        let random = words[Math.floor(Math.random() * words.length)];
-        let WordIndexOf = words.indexOf(random);
-        words.splice(WordIndexOf,1);
-        theWord.innerHTML=random;
-        upcomingWords.innerHTML="";
-        for (let index = 0; index < words.length; index++) {
-            let div = document.createElement("div");
-            let text = document.createTextNode(words[index]);
-            div.appendChild(text);
-            upcomingWords.appendChild(div)
-            
-        }
-        startplay();
+function checkAnswer(selectedOption) {
+    const question = questions[currentQuestionIndex];
+    if (selectedOption === question.correctOption) {
+        score++;
+        scoreElement.textContent = score;
     }
     
-    function startplay(){
-        timeLeftSpan.innerHTML=defaultNormalValue;
-        let start = setInterval(()=>{
-            timeLeftSpan.innerHTML--;
-            if(timeLeftSpan.innerHTML === "0"){
-                clearInterval(start)
-                if (theWord.innerHTML.toLowerCase() === input.value.toLowerCase()) {
-                    input.value ='';
-                    scoreGot.innerHTML++;
-                    if(words.length > 0){
-                        getAttribute()
-                    }else{
-                        let span= document.createElement("span");
-                        span.className="good";
-                        let textSpan = document.createTextNode("Congratz");
-                        span.appendChild(textSpan);
-                        finishMessage.appendChild(span);
-                        upcomingWords.remove()
-                    }
-                }else{
-                    let res =document.createElement("div");
-                    res.className="reset";
-                    let textReset = document.createTextNode("Reset");
-                    res.appendChild(textReset);
-                    // resetGame.appendChild(reset);
-                    finishMessage.appendChild(res)
-                }
-            }
-        },1000)
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        displayQuestion(currentQuestionIndex);
+    } else {
+        alert(`انتهت اللعبة! النقاط الإجمالية: ${score}`);
     }
-setTimeout(()=>{
-    document.querySelector(".loading").remove()
-},7000)
+}
 
+optionAElement.addEventListener('click', () => checkAnswer(optionAElement.textContent));
+optionBElement.addEventListener('click', () => checkAnswer(optionBElement.textContent));
+optionCElement.addEventListener('click', () => checkAnswer(optionCElement.textContent));
+optionDElement.addEventListener('click', () => checkAnswer(optionDElement.textContent));
+optionEElement.addEventListener('click', () => checkAnswer(optionEElement.textContent));
+optionFElement.addEventListener('click', () => checkAnswer(optionFElement.textContent));
+optionGElement.addEventListener('click', () => checkAnswer(optionGElement.textContent));
+optionHElement.addEventListener('click', () => checkAnswer(optionHElement.textContent));
+optionKElement.addEventListener('click', () => checkAnswer(optionKElement.textContent));
 
+displayQuestion(currentQuestionIndex);
